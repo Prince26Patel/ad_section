8703698ac2e545035b3e4aa9783c92db2fd677eb
Prince Patel <155377199+Prince26Patel@users.noreply.github.com>
2024-01-30 21:32:24 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import CarouselPage from './adSection/CarouselPage';
+function App() {
+  return (
+    <div >
+      <CarouselPage/>
+    </div>
+  );
+}
+
+export default App;
